c7a9cc90c727705c9e7452d0ad80e9e02b847421
Mónika Lombos <lombos.monika@gmail.com>
2016-12-14 11:31:56 +0100

    Create test's skeleton

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,25 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by lombocska on 2016.12.14..
+ */
+public class CardTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getValue() throws Exception {
+
+    }
+
+}
